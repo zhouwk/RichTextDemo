108d83cfb210c778d473b2a5fd46bcc161946f45
zhouwk <me_nerd@163.com>
2018-12-21 13:07:52 +0800

    test

--- RichTextDemo/AppDelegate.swift
+++ RichTextDemo/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         setupWindow()
+        
+        print("")
         return true
     }
     
